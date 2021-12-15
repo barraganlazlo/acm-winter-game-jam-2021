c26c53a3bb03546a5bf0c28d276656bcd7e80124
lazlo barragan <lazlo.barragan@gmail.com>
2021-12-15 13:33:50 +0100

    character movement and shoot + import sprites + setup small snowman

---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const small_snowball_player := preload("res://scenes/projectiles/small_snowball_player.tscn")
+const small_snowball_enemy  := preload("res://scenes/projectiles/small_snowball_enemy.tscn")
+
+var gamepad_connected:=false
+
